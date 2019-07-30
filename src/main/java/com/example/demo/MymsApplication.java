150b6ccecd5e4e8436491df708b614936e7db77a
Mastan <mastanvali2600@gmail.com>
2019-07-31 03:26:19 +0800

    first commit

---
+++ src/main/java/com/example/demo/MymsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymsApplication.class, args);
+	}
+
+}
